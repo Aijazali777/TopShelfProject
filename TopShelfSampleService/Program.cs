f12b8891ec39310c02992aa550f290887f2ee4ef
Aijaz Ali <aijazalee889@gmail.com>
2025-12-23 21:47:10 +0500

    added files

---
+++ TopShelfSampleService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TopShelfSampleService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<TestService>(s =>
+                {
+                    s.ConstructUsing(() => new TestService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("TopShelfSampleService");
+                x.SetDisplayName("TopShelf Sample Service");
+                x.SetDescription("Simple Topshelf test service using .NET Framework");
+
+                x.StartAutomatically();
+            });
+        }
+    }
+}
---
+++ TopShelfSampleService/TestService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TopShelfSampleService
+{
+    public class TestService
+    {
+        private Timer _timer;
+
+        public bool Start()
+        {
+            _timer = new Timer(5000); // 5 seconds
+            _timer.Elapsed += (s, e) =>
+            {
+                Console.WriteLine($"Service running at {DateTime.Now}");
+            };
+            _timer.Start();
+
+            return true;
+        }
+
+        public bool Stop()
+        {
+            _timer?.Stop();
+            _timer?.Dispose();
+            return true;
+        }
+    }
+}
